a91bbaed46a0ea7257058e1793df1f9813fe068e
Christoph Bartschat <christoph@barts.chat>
2026-08-05 23:06:15 -0700

    9_0

--- src/digits.rs
+++ src/digits.rs
@@ -324,9 +324,26 @@ pub fn render_8_9(ctx: &mut GContext, bounds: &DigitBounds, progress: i32) {
     TRANSITION.render(ctx, bounds, progress);
 }
 
-// pub fn render_9_0(ctx: &mut GContext, bounds: &DigitBounds,  progress: i32) {
+pub fn render_9_0(ctx: &mut GContext, bounds: &DigitBounds, progress: i32) {
+    static ALWAYS: Transition = Transition::def(&[Stroke::forward(
+        0,
+        &[
+            Seg::def(Pos::Tl, Dir::R),
+            Seg::def(Pos::Tr, Dir::D),
+            Seg::def(Pos::Tl, Dir::Hd),
+        ],
+    )]);
+    static TRANSITION: Transition = Transition::def(&[
+        Stroke::back(0, &[Seg::def(Pos::Cl, Dir::R)]),
+        Stroke::forward(
+            3 * TIME_SCALE,
+            &[Seg::def(Pos::Br, Dir::L), Seg::def(Pos::Bl, Dir::Hu)],
+        ),
+    ]);
 
-// }
+    ALWAYS.render(ctx, bounds, MAX_TIME);
+    TRANSITION.render(ctx, bounds, progress);
+}
 
 pub fn render_1_0(ctx: &mut GContext, bounds: &DigitBounds, progress: i32) {
     static TRANSITION: Transition = Transition::def(&[
--- src/render.rs
+++ src/render.rs
@@ -75,9 +75,9 @@ pub fn render_interpolated_digit(
         InterpolatedDigit::Change(old, new) if *old == 8 && *new == 9 => {
             render_8_9(ctx, bounds, progress)
         }
-        // InterpolatedDigit::Change(old, new) if *old == 9 && *new == 0 => {
-        //     render_9_0(ctx, bounds, progress)
-        // }
+        InterpolatedDigit::Change(old, new) if *old == 9 && *new == 0 => {
+            render_9_0(ctx, bounds, progress)
+        }
         // InterpolatedDigit::Change(old, new) if *old == 2 && *new == 1 => {
         //     render_2_1(ctx, bounds, progress)
         // }
